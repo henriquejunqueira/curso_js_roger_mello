9ac7820622c08cddf6e00244b1d250685da69e7c
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-11-26 13:27:02 -0300

    Exercício 01

---
+++ modulo01/aula01/app.js
@@ -0,0 +1,14 @@
+let age = 31;
+let currentYear = 2024;
+
+console.log(age, currentYear);
+
+age = 32;
+console.log(age);
+
+const points = 100;
+// points = 101; // não pode-se reatribuir valor à constantes
+console.log(points);
+
+var score = 75;
+console.log(score);
