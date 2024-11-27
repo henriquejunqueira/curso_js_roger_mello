9154c1be59b13c29a6a09d26d6b8ef0336e88efa
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-11-27 15:06:06 -0300

    Tipos de operadores

---
+++ modulo01/aula04/app.js
@@ -0,0 +1,33 @@
+// * inteiros e decimais
+const radius = 10;
+const pi = 3.14;
+
+// * operadores aritméticos
+// const reminder = 5 % 2; // resto 1
+// const reminder = 4 % 2; // resto 0
+const area = pi * radius ** 2;
+
+// * ordem de operações
+const crazyOperation = 5 * (10 - 3) ** 2;
+
+// * operadores de incremento e decremento
+let postLikes = 10;
+
+// postLikes = postLikes + 1;
+// postLikes++;
+// postLikes--;
+
+// * operadores addition, subtraction, multiplication e division assignment
+// postLikes = postLikes + 10;
+// postLikes += 10;
+// postLikes -= 5;
+// postLikes *= 3;
+// postLikes /= 2;
+
+// * NaN - not a number
+// console.log(7 / 'oi');
+
+// * concatenação de string com número
+const likesMessage = 'O post tem ' + postLikes + ' likes.';
+
+console.log(likesMessage);
